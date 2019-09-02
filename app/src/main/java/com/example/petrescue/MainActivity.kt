48d304fb7d97b38faefe787816b6fd65715ba521
lukile <lucile.1988.ls@gmail.com>
2019-09-02 17:42:55 +0200

    Create home page and change navigation menu

--- app/src/main/java/com/example/petrescue/MainActivity.kt
+++ app/src/main/java/com/example/petrescue/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.petrescue
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(){
     private lateinit var drawer: DrawerLayout
@@ -25,6 +27,25 @@ class MainActivity : AppCompatActivity(){
         drawer.addDrawerListener(actionBar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { item ->
+            item.isChecked = true
+            drawer.closeDrawers()
+
+            when(item.itemId) {
+                R.id.nav_profile -> {
+                    Toast.makeText(this, "Profil", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_commercials -> {
+                    Toast.makeText(this, "Annonces", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_post -> {
+                    Toast.makeText(this, "Poster une annonce", Toast.LENGTH_LONG).show()
+                }
+            }
+            true
+        }
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
